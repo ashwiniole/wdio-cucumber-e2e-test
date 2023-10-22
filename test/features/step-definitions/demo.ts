82ae5514b719874ab2b10cf4bac86ef5448b11d3
ashwiniole <ashwininl0019@gmail.com>
2023-10-22 22:16:58 +1100

    adding checkbox and windows handling

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -30,7 +30,7 @@ Then(/^URL should match (.*)$/, async function (expectedURL) {
  * Web Interactions
  */
 Given(/^The web page is launched$/, async function () {
-  await browser.url("/dropdown");
+  await browser.url("/windows");
   await browser.setTimeout({ implicit: 15000, pageLoad: 10000 });
   await browser.maximizeWindow();
 });
@@ -44,15 +44,15 @@ When(/^Perform web interactions$/, async function () {
    * 3. Click and type
    * 4. Slow typing
    */
- // let num = 12345;
+  // let num = 12345;
   //let strNum = num.toString();
   //let inputBox = await $(`[type="number"]`);
   //await inputBox.setValue(strNum)
   //await inputBox.click();
   //for (let i = 0; i < strNum.length; i++) {
-    //let charStr = strNum.charAt(i);
-    //await browser.pause(1000);
-    //await browser.keys(charStr);
+  //let charStr = strNum.charAt(i);
+  //await browser.pause(1000);
+  //await browser.keys(charStr);
   //}
 
   /**
@@ -63,7 +63,7 @@ When(/^Perform web interactions$/, async function () {
    * 3. Get a list of options
    */
 
-  let dropDown = await $(`//select/option[@selected="selected"]`)
+  /*let dropDown = await $(`//select/option[@selected="selected"]`)
   let text = await dropDown.getText()
   chai.expect(text).to.equal("Please select an option")
 
@@ -80,6 +80,66 @@ When(/^Perform web interactions$/, async function () {
     arr.push(text)
     console.log(text);
   }
-  console.log(`Options array: ${arr}`);
+  console.log(`Options array: ${arr}`);*/
+
+  /**
+   * 3. Checkbox
+   * Actions:
+   * 1. Select an option
+   * 2. Unselect an option (if selected)
+   * 3. Assert if option is selected
+   * 4. Select all options
+   */
+  /*let chkBox = await $(`//form[@id="checkboxes"]/input`);
+  if (!(await chkBox.isSelected())) {
+    await chkBox.click();
+  }
+
+  let chkBox2 = await $(`//form[@id="checkboxes"]/input[2]`);
+  let isChecked = await chkBox2.isSelected();
+  chai.expect(isChecked).to.be.true;
+  await browser.debug();
+
+  let chkBoxArr = await $$(`//form[@id="checkboxes"]/input`);
+  for (let i = 0; i < chkBoxArr.length; i++) {
+    let ele = chkBoxArr[i];
+    if (!(await ele.isSelected())) {
+      ele.click();
+    }
+  }*/
+
+  /**
+   * 4. Windows handling
+   * Steps:
+   * 1. Launch the browser
+   * 2. Open another windows
+   * 3. Switch to the window based on title
+   * 4. Switch back to the main window
+   */
+  let clkHere = $(`=Click Here`)
+  let eleSelenium = $(`=Elemental Selenium`)
+  await clkHere.click()
+  await eleSelenium.click()
+  let currentWinTitle = await browser.getTitle();
+  let parentWinHandle = await browser.getWindowHandle()
+  console.log(`Current title: ${currentWinTitle}`);
+
+  let winHandles = await browser.getWindowHandles()
+  for(let i=0;i<winHandles.length;i++){
+    console.log(`window handles: ${winHandles[i]}`);
+    await browser.switchToWindow(winHandles[i])
+    currentWinTitle = await browser.getTitle()
+    if(currentWinTitle === "Elemental Selenium | Elemental Selenium"){
+      await browser.switchToWindow(winHandles[i])
+      let headerText = await (await $(`<h1>`)).getText()
+      console.log(`header text: ${headerText}`);
+      break
+    }
+  }
+
+  await browser.switchToWindow(parentWinHandle)
+  let headerTextParent = await (await $(`<h3>`)).getText()
+  console.log(`header text parent: ${headerTextParent}`);
   await browser.debug()
+
 });
