77bf729bdac215d6fb4ae802f0bd9394df2a78ce
ashwiniole <ashwininl0019@gmail.com>
2023-10-22 18:57:52 +1100

    add dropdown handling

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -30,7 +30,7 @@ Then(/^URL should match (.*)$/, async function (expectedURL) {
  * Web Interactions
  */
 Given(/^The web page is launched$/, async function () {
-  await browser.url("/inputs");
+  await browser.url("/dropdown");
   await browser.setTimeout({ implicit: 15000, pageLoad: 10000 });
   await browser.maximizeWindow();
 });
@@ -44,14 +44,42 @@ When(/^Perform web interactions$/, async function () {
    * 3. Click and type
    * 4. Slow typing
    */
-  let num = 12345;
-  let strNum = num.toString();
-  let inputBox = await $(`[type="number"]`);
+ // let num = 12345;
+  //let strNum = num.toString();
+  //let inputBox = await $(`[type="number"]`);
   //await inputBox.setValue(strNum)
-  await inputBox.click();
-  for (let i = 0; i < strNum.length; i++) {
-    let charStr = strNum.charAt(i);
-    await browser.pause(1000);
-    await browser.keys(charStr);
+  //await inputBox.click();
+  //for (let i = 0; i < strNum.length; i++) {
+    //let charStr = strNum.charAt(i);
+    //await browser.pause(1000);
+    //await browser.keys(charStr);
+  //}
+
+  /**
+   * 2. Dropdown
+   * Actions
+   * 1. Assert default option is selected
+   * 2. Select by attribute, text, index
+   * 3. Get a list of options
+   */
+
+  let dropDown = await $(`//select/option[@selected="selected"]`)
+  let text = await dropDown.getText()
+  chai.expect(text).to.equal("Please select an option")
+
+  let dropDownEle = await $(`#dropdown`)
+  //await dropDownEle.selectByAttribute("value","2")
+  //await dropDownEle.selectByIndex(2)
+  await dropDownEle.selectByVisibleText("Option 2")
+
+  let dropDownArr = await $$(`//select/option`)
+  let arr = []
+  for (let i=0;i<dropDownArr.length;i++){
+    let ele = dropDownArr[i]
+    let text = await ele.getText()
+    arr.push(text)
+    console.log(text);
   }
+  console.log(`Options array: ${arr}`);
+  await browser.debug()
 });
