5d1dfc7d19e53e56018a96e730e2930746995a8e
ashwiniole <ashwininl0019@gmail.com>
2023-11-25 19:31:32 +1100

    add Web interactions

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -30,7 +30,7 @@ Then(/^URL should match (.*)$/, async function (expectedURL) {
  * Web Interactions
  */
 Given(/^The web page is launched$/, async function () {
-  await browser.url("/frames");
+  await browser.url("https://www.amazon.com.au/");
   await browser.setTimeout({ implicit: 15000, pageLoad: 10000 });
   await browser.maximizeWindow();
 });
@@ -185,4 +185,124 @@ await browser.switchToParentFrame()
    * 1. scrollIntoView
    */
   
+  /**
+   * Table interactions
+   * 1. Check number of rows and columns
+   * 2. Get whole table data
+   * 3. Get single row
+   * 4. Get single column
+   * 5. Get single cell value
+   */
+  //1. Check number of rows and columns
+  /*let rowCount = await $$(`//table[@id="table1"]/tbody/tr`).length
+  let colCount = await $$(`//table[@id="table1"]/thead/tr/th`).length
+  console.log(`>> Row count: ${rowCount}`);
+  chai.expect(rowCount).to.be.equal(4)
+  console.log(`>> Column count: ${colCount}`);
+  chai.expect(colCount).to.be.equal(6)
+
+  //2. Get whole table data
+  let arrObj = []
+  for (let i=1;i<=rowCount;i++){
+    //key-value pairs
+    let personObj = {
+      lastName: "",
+      firstName: "",
+      email: "",
+      due: "",
+      website: ""
+    }
+    for(let j=1;j<=colCount;j++){
+      let cellVal = await (await $(`//table[@id="table1"]/tbody/tr[${i}]/td[${j}]`)).getText()
+      //console.log(`>> Cell value: ${cellVal}`);
+      if(j===1) personObj.lastName = cellVal
+      if(j===2) personObj.firstName = cellVal
+      if(j===3) personObj.email = cellVal
+      if(j===4) personObj.due = cellVal
+      if(j===5) personObj.website = cellVal
+    }
+    arrObj.push(personObj)
+  }
+  console.log(`Whole table: ${JSON.stringify(arrObj)}`);
+
+  //3. Get single row
+  let arr = []
+  for(let k=1; k<=rowCount;k++){
+    let pplObj = {
+      lastName: "",
+      firstName: "",
+      email: "",
+      due: "",
+      website: ""
+    }
+    for (let l=1;l<=colCount;l++){
+      let celVal = await (await $(`//table[@id="table1"]/tbody/tr[${k}]/td[${l}]`)).getText()
+      let firstName = await (await $(`//table[@id="table1"]/tbody/tr[${k}]/td[${2}]`)).getText()
+      if(firstName === "Jason"){
+        if(l===1) pplObj.lastName = celVal
+        if(l===2) pplObj.firstName = celVal
+        if(l===3) pplObj.email = celVal
+        if(l===4) pplObj.due = celVal
+        if(l===5) pplObj.website = celVal
+      }
+    }
+    if(pplObj.firstName){
+      arr.push(pplObj)
+    }
+    
+  }
+  console.log(`Web table: ${JSON.stringify(arr)}`);
+
+  //4. Get single column
+   let singleColArr = []
+   for(let m=1;m<=rowCount;m++){
+    let cellVal = await (await $(`//table[@id="table1"]/tbody/tr[${m}]/td[${4}]`)).getText()
+    singleColArr.push(cellVal)
+   }
+   console.log(`>> Single col values: ${singleColArr}`);
+  
+  //5. Get single cell value [based on another call]
+  let singleCellValue =[]
+  for (let n=1; n<=rowCount;n++){
+    let price = await (await $(`//table[@id="table1"]/tbody/tr[${n}]/td[4]`)).getText()
+    let lastName = await (await $(`//table[@id="table1"]/tbody/tr[${n}]/td[2]`)).getText()
+    
+    if(+(price.replace("$",""))>50){
+      singleCellValue.push(lastName)
+    }
+  }
+  console.log(`single cell value: ${singleCellValue}`); */
+
+  /**
+   * SCROLLING
+   * VISIBLE PORTION
+   * Windows object:
+   * 1. scrollBy
+   * Y -> [-]window.innerHeight
+   */
+  //Scroll down
+  await browser.execute(() => {
+    window.scrollBy(0,window.innerHeight)
+  })
+  await browser.pause(1000)
+  //scroll top
+  await browser.execute(() => {
+    window.scrollBy(0,-window.innerHeight)
+  })
+  await browser.pause(1000)
+  console.log(`Scrolled down`);
+  
+  /**
+   * INVISIBLE PORTION
+   * windows object:
+   * 1. scrollTo
+   * Y -> document.body.scrollTop/scrollHeight
+   */
+  await browser.execute(() => {
+    window.scrollTo(0,document.body.scrollHeight)
+  })
+
+  await browser.execute(() => {
+    window.scrollTo(0, document.body.scrollTop)
+  })
 });
